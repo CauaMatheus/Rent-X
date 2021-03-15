77c5dd27ede481d239fdc8e159c9035139e3e035
CauaMatheus <caua10000@gmail.com>
2021-03-15 11:03:26 -0300

    VSCode Debug

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => response.json({ message: 'Hello World!' }));
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+  return response.json(name);
+});
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
