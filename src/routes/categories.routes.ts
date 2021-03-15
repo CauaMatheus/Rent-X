72a677b50de703d4f0495e4f39aaf587403de3cc
CauaMatheus <caua10000@gmail.com>
2021-03-15 11:54:29 -0300

    Create Categories Repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,15 @@
 import { Request, Response, Router } from 'express';
 
-import { Category } from '../models/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
-
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
+  const categoriesRepository = new CategoriesRepository();
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-  return response.status(201).json(category);
+  categoriesRepository.create({ name, description });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
