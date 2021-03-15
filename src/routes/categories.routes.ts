48cfe4119aa79dcb6cbf99a4b4264e0506812b55
CauaMatheus <caua10000@gmail.com>
2021-03-15 11:20:38 -0300

    Adding UUID & Date to Categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,19 @@
 import { Request, Response, Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request: Request, response: Response) => {
+categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  categories.push({ name, description });
+  categories.push({
+    name,
+    description,
+    id: uuid(),
+    created_at: new Date(),
+  });
   return response.status(201).send();
 });
 
