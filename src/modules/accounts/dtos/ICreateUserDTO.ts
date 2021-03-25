9ffc82eaa2d3b0516cde40347e0a0a316367e2f8
CauaMatheus <caua10000@gmail.com>
2021-03-25 09:54:28 -0300

    Create User

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,7 @@
+interface ICreateUserDTO{
+  email:string
+  name: string
+  password:string
+  driver_license: string
+}
+export { ICreateUserDTO };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,15 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { ICategoryRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository', UsersRepository,
+);
 container.registerSingleton<ICategoryRepository>(
   'CategoriesRepository', CategoriesRepository,
 );
