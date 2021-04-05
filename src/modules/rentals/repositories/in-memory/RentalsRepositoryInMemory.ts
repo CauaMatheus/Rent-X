6aad1abe1cc2def112e26e4cd7b187739cc8b0ff
CauaMatheus <caua10000@gmail.com>
2021-04-05 11:05:28 -0300

    InMemoryRentalsRepository

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -7,25 +7,30 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   private repository:Rental[] = []
 
   async create({
+    id,
     car_id,
+    user_id,
     end_date,
     expected_return_date,
-    user_id,
-    id,
     total,
-    updated_at,
   }:ICreateRentalsDTO):Promise<Rental> {
-    const rental = new Rental();
-    Object.assign(rental, {
-      car_id,
-      end_date,
-      expected_return_date,
-      user_id,
-      start_date: new Date(),
-    });
+    if (!id) {
+      const rental = new Rental();
+      Object.assign(rental, {
+        car_id,
+        expected_return_date,
+        user_id,
+        start_date: new Date(),
+      });
 
-    this.repository.push(rental);
-    return rental;
+      this.repository.push(rental);
+      return rental;
+    }
+
+    const rentalIndex = this.repository.findIndex((rental) => rental.id === id);
+    this.repository[rentalIndex].end_date = end_date;
+    this.repository[rentalIndex].total = total;
+    return this.repository[rentalIndex];
   }
 
   async findOpenRentalByCarId(id:string): Promise<Rental> {
@@ -43,6 +48,11 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   async findById(id:string): Promise<Rental> {
     const rental = this.repository.find((rental) => rental.id === id);
     return rental;
+  }
+
+  async findByUserId(id:string): Promise<Rental[]> {
+    const rentals = this.repository.filter((rental) => rental.user_id === id);
+    return rentals;
   }
 }
 
