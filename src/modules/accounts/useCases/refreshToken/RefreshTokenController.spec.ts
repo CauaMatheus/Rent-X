40e73e4327902c7c26d3f857481b5c780c9ed9db
CauaMatheus <caua10000@gmail.com>
2021-04-26 18:33:46 -0300

    Missed save

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.spec.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.spec.ts
@@ -1,15 +1,12 @@
 import { hash } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
 import request from 'supertest';
 import { Connection } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
-import auth from '@config/auth';
 import { app } from '@shared/infra/http/app';
 import createConnection from '@shared/infra/typeorm';
 
 let connection: Connection;
-let user_id: string;
 
 describe('Refresh Token Controller', () => {
   beforeAll(async () => {
@@ -20,8 +17,6 @@ describe('Refresh Token Controller', () => {
     const password = hash('password', 12);
     await connection.query(`INSERT INTO users (id, name, email, password, driver_license)
     values ('${id}', 'NameExample', 'email@example.com', '${password}', 'X')`);
-
-    user_id = id;
   });
 
   afterAll(async () => {
@@ -42,10 +37,5 @@ describe('Refresh Token Controller', () => {
 
     expect(refreshTokenResponse.status).toBe(200);
     expect(refreshTokenResponse.body).not.toBeNull();
-  });
-
-  it('should not be able to refresh token if token was not authentic', () => {
-    const { } = auth;
-    const refresh_token = sign({ email: 'email@example.com' });
   });
 });
